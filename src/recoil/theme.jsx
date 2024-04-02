8afc279f3e99c17893010c9e235da86e79e458b3
shimseohyun <shim020924@gmail.com>
2024-04-02 16:25:01 +0900

    ✨ Feat - add description

--- src/recoil/theme.jsx
+++ src/recoil/theme.jsx
@@ -4,6 +4,7 @@ import moment from "moment";
 export const getTheme = () => {
   const nowTime = parseInt(moment().format("H"));
 
+  //현재 시간이 오전 6시 부터, 오후 7시 사이라면 낮을 반환
   return nowTime > 6 && nowTime < 19 ? "DAY" : "NIGHT";
 };
 
