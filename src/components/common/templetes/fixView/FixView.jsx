bdc865d274516cdf2743fd20c9aedfe19e3473ec
shimseohyun <shim020924@gmail.com>
2024-04-14 00:10:32 +0900

    ✨ Feat - add Background

--- src/components/common/templetes/fixView/FixView.jsx
+++ src/components/common/templetes/fixView/FixView.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import styled from "styled-components";
 import { useRecoilValue } from "recoil";
 import { heightSize } from "../../../../recoil/heightSize";
+import { theme } from "../../../../recoil/theme";
 
 const FixViewWrapper = styled.div`
   width: 100%;
@@ -17,13 +18,20 @@ const FixViewWrapper = styled.div`
 
   padding: 0px 16px;
   min-height: ${props => props.$heightSize}px;
+
+  background-size: cover;
+  background-image: ${props => `url(/img/fixBg/${props.$theme}.png)`};
 `;
 
 function FixView({ children }) {
+  const themeValue = useRecoilValue(theme);
+
   const heightSizeValue = useRecoilValue(heightSize);
 
   return (
-    <FixViewWrapper $heightSize={heightSizeValue}>{children}</FixViewWrapper>
+    <FixViewWrapper $theme={themeValue} $heightSize={heightSizeValue}>
+      {children}
+    </FixViewWrapper>
   );
 }
 
