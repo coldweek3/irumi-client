525f1c72ecf9a6d9dc8ec6d7df976b5d148b9920
coldweek3 <ohchanju3@naver.com>
2024-04-16 11:48:27 +0900

    :lipstick: fix: backBtn, header em-> px 변경

--- src/components/common/atoms/button/BackBtn.jsx
+++ src/components/common/atoms/button/BackBtn.jsx
@@ -3,18 +3,17 @@ import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 
 const BackBtnContainer = styled.div`
-  height: 4em;
+  height: 100px;
   display: flex;
   align-items: center;
   position: absolute;
-  margin-top: 1em;
   z-index: 9;
+  width: 100%;
 `;
 
 const BackBtnImg = styled.img`
   width: 2em;
-  height: 1.5em;
-  margin-left: 1.5em;
+  margin-left: 24px;
   cursor: pointer;
 `;
 
@@ -27,7 +26,7 @@ function BackBtn() {
 
   return (
     <BackBtnContainer onClick={handleBackClick}>
-      <BackBtnImg img src="/common/backBtnImg.png" />
+      <BackBtnImg src="/common/backBtnImg.png" />
     </BackBtnContainer>
   );
 }
--- src/components/common/molecules/header/header.jsx
+++ src/components/common/molecules/header/header.jsx
@@ -3,21 +3,20 @@ import styled from "styled-components";
 import BackBtn from "../../atoms/button/BackBtn";
 
 const HeaderWrapper = styled.div`
-  height: 4em;
+  height: 100px;
   flex-direction: row;
   display: flex;
 `;
 
 const HeaderTitle = styled.div`
-  height: 4em;
+  height: 100px;
   width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
   position: absolute;
   color: white;
-  font-size: 1em;
-  margin-top: 1em;
+  font-size: 20px;
 `;
 
 function Header(props) {
