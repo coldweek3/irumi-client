a772adca1f3d88b238b7c60281bb770e35bfee71
shimseohyun <shim020924@gmail.com>
2024-04-19 18:52:47 +0900

    ✏️ Fix - router, lanternCard error
    router에 경로가 중복 설정되어있는 것을 변경했습니다. lantern card 배경이미지 경로를 재설정 했습니다.

--- src/apis/api/lantern.js
+++ src/apis/api/lantern.js
@@ -3,6 +3,7 @@ import { API } from "../utils";
 export const getLanterns = async type => {
   try {
     const result = await API.get(`api/lanterns/${type}`);
+    console.log(result);
     return { type: "clear", data: result.data };
   } catch (error) {
     console.log("에러발생");
--- src/router.jsx
+++ src/router.jsx
@@ -4,15 +4,6 @@ import App from "./App";
 import LanternWrite from "./pages/Lantern/LanternWrite";
 import LanternColor from "./pages/Lantern/LanternColorPage";
 
-import MailWrite from "./pages/mailWirte/mailWrite";
-import MainPage from "./pages/main/MainPage";
-import LanternsPage from "./pages/lanterns/LanternsPage";
-import LanternsSearchPage from "./pages/lanterns/LanternsSearchPage";
-import LanternDetail from "./pages/Lantern/LanternDetail";
-
-import LanternWrite from "./pages/Lantern/LanternWrite";
-import LanternColor from "./pages/Lantern/LanternColorPage";
-
 import MainPage from "./pages/main/MainPage";
 import LanternsPage from "./pages/lanterns/LanternsPage";
 import LanternsSearchPage from "./pages/lanterns/LanternsSearchPage";
@@ -20,11 +11,6 @@ import LampMain from "./pages/lamp/LampMain";
 import FirstthemeWrite from "./pages/lamp/FirstthemeWrite";
 import SecondthemeWrite from "./pages/lamp/SecondthemeWrite";
 import ThirdthemeWrite from "./pages/lamp/ThirdthemeWrite";
-import FlyLamp from "./pages/lamp/FlyLamp";
-import FortuneIntro from "./pages/fotrune/FortuneIntro";
-import FortuneLeaf from "./pages/fotrune/FotuneLeaf";
-import MyDetail from "./pages/fotrune/MyDetail";
-
 
 const router = createBrowserRouter([
   {
@@ -41,7 +27,6 @@ const router = createBrowserRouter([
       { path: "/firstthemeWrite", element: <FirstthemeWrite /> },
       { path: "/secondthemeWrite", element: <SecondthemeWrite /> },
       { path: "/thirdthemeWrite", element: <ThirdthemeWrite /> }
-
     ]
   }
 ]);
