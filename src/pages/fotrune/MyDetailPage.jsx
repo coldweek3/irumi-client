034edbf5a72c2008c875455b8125b5cf859b6def
kickcha <chasm030517@gmail.com>
2024-04-22 15:26:16 +0900

    💄: 포춘 이슈 수정 (이미지다운로드 수정 필요)

--- src/pages/fotrune/MyDetailPage.jsx
+++ src/pages/fotrune/MyDetailPage.jsx
@@ -4,11 +4,9 @@ import { useParams } from "react-router-dom";
 import FixView from "../../components/common/templetes/fixView/FixView";
 import MyBtn from "../../components/fortune/atoms/MyBtn";
 import { fetchLanternData } from "../../apis/api/lanternDetail";
-
+import html2canvas from "html2canvas";
 
 const DetailLanternWrapper = styled.div`
-  /* width: 84%; */
-  /* height: auto; */
   position: absolute;
   display: flex;
   flex-direction: column;
@@ -17,7 +15,6 @@ const DetailLanternWrapper = styled.div`
   left: 50%;
   top: 41%;
   transform: translate(-50%, -50%);
-  position: absolute;
 `;
 
 const DetailLanternImg = styled.img`
@@ -27,42 +24,76 @@ const DetailLanternImg = styled.img`
 
 const TitleSec = styled.div`
   position: absolute;
-  top: 60%;
-  width: 44%;
+  top: 56%;
+  width: 33%;
   color: #5b3a1a;
   font-size: 16px;
   font-weight: 400;
   display: flex;
   justify-content: center;
   align-items: center;
-  /* border: 1px solid brown; */
 `;
 
-const ContentSec = styled(TitleSec)`
-  top: 65%;
+const ContentSec = styled.div`
+  position: absolute;
+  display: flex;
+  top: 62%;
+  width: 33%;
+  color: #5b3a1a;
   font-size: 14px;
+  justify-content: center;
+  align-items: center;
   line-height: 130%;
-  /* border: 1px solid white; */
 `;
 
 function MyDetailPage() {
   const LinkRef = useRef();
+  const LanternRef = useRef(); // 이미지 요소를 참조하는 Ref
+  const TitleSecRef = useRef(); // 제목 요소를 참조하는 Ref
+  const ContentSecRef = useRef(); // 내용 요소를 참조하는 Ref
+  const DetailLanternWrapperRef = useRef(); // DetailLanternWrapper 요소를 참조하는 Ref
   const { detailId } = useParams();
   const [lanternData, setLanternData] = useState(null);
 
   useEffect(() => {
     const getLanternDetail = async () => {
       try {
-
         const data = await fetchLanternData(detailId);
         setLanternData(data);
-        console.log(data);
       } catch (error) {
         console.error("Error fetching lantern detail:", error);
       }
     };
     getLanternDetail();
   }, [detailId]);
+
+  const handleDownload = async () => {
+    if (!LanternRef.current || !DetailLanternWrapperRef.current) return;
+
+    try {
+      const canvas = await html2canvas(DetailLanternWrapperRef.current); // DetailLanternWrapper 캡쳐
+      const ctx = canvas.getContext("2d");
+
+      // 제목 텍스트 가져오기 및 그리기
+      const titleText = lanternData.nickname;
+      ctx.fillText(titleText, 10, 250);
+
+      // 내용 텍스트 가져오기 및 그리기
+      const contentText = lanternData.content;
+      ctx.fillText(contentText, 10, 270);
+
+      const imageUrl = canvas.toDataURL();
+      const a = document.createElement("a");
+      a.href = imageUrl;
+      a.download = "당신의_연등.png";
+
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    } catch (error) {
+      console.error("Error capturing image:", error);
+    }
+  };
 
   return (
     <FixView>
@@ -72,17 +103,23 @@ function MyDetailPage() {
         style={{ position: "fixed", top: "-123px" }}
       />
       {lanternData && (
-        <DetailLanternWrapper>
+        <DetailLanternWrapper ref={DetailLanternWrapperRef}>
           <DetailLanternImg
+            ref={LanternRef}
             src={`/img/lanternCard/${lanternData.lanternColor}_${lanternData.light_bool}.png`}
           />
-
-          <TitleSec>{lanternData.nickname}</TitleSec>
-          <ContentSec>{lanternData.content}</ContentSec>
+          <TitleSec ref={TitleSecRef}>{lanternData.nickname}</TitleSec>
+          <ContentSec ref={ContentSecRef}>{lanternData.content}</ContentSec>
         </DetailLanternWrapper>
       )}
-
-      <MyBtn />
+      {lanternData && (
+        <MyBtn
+          handleDownload={handleDownload}
+          LanternRef={LanternRef}
+          TitleSecRef={TitleSecRef}
+          ContentSecRef={ContentSecRef}
+        />
+      )}
     </FixView>
   );
 }
